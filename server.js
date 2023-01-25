4c0926d89d1548fa8c0b23a0ce459fddeea9cfa3
Elia <eliavencato02@gmail.com>
2023-01-25 16:56:11 +0100

    update 1.1

--- server.js
+++ server.js
@@ -2,26 +2,29 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const database = require ( 'mongoose' );
-database.set('strictQuery', true);
-database.connect(
-    process.env.MONGODB_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-        if(err) return console.log("Error: ", err);
-        console.log("MongoDB Connection -- Ready state is:", database.connection.readyState); 
-    }
-);
-
-
 const routes = require('./routes/ingrediente');
+const mongoose = require ( 'mongoose' );
+mongoose.set('strictQuery', true);
+
+app.use(express.json());
 app.use('/', routes);
 
+// Connessione al DataBase
+try{
+    mongoose.connect(
+        process.env.MONGODB_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err) => {
+            if(err) return console.log("Error: ", err);
+            console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState); 
+        }
+    );
+} catch (e){
+    console.log("DB not connected")
+}
 
-
+// listener Porta
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('Your app is listen on port ' + 
     listener.address().port)
 })
-
-module.export =  database;
